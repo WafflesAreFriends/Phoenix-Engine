7f8e84f7220a54a1445da62255a4efdaf8f524be
WafflesAreFriends <johnfmunar@yahoo.com>
2019-09-28 22:50:42 -0700

    Added Refs for shared_ptr and scopes for unique_ptr

--- Phoenix/src/Phoenix/Core.h
+++ Phoenix/src/Phoenix/Core.h
@@ -27,3 +27,12 @@
 #endif
 
 #define BIT(x) (1 << x)
+
+namespace Phoenix {
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+}
